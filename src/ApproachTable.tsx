f0147f5708673fcc5c4078c54b824a31833fa676
Ammar Askar <ammar@ammaraskar.com>
2024-08-06 01:48:48 -0400

    Use material react table instead of tanstack directly

--- src/ApproachTable.tsx
+++ src/ApproachTable.tsx
@@ -1,10 +1,13 @@
-import Table from 'react-bootstrap/Table';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
-import Badge from 'react-bootstrap/Badge';
-import Stack from 'react-bootstrap/Stack';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import Tooltip from '@mui/material/Tooltip';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import {
+    MRT_Table,
+    useMaterialReactTable,
+    createMRTColumnHelper,
+} from 'material-react-table';
 
-import { createColumnHelper, useReactTable, getCoreRowModel, flexRender } from '@tanstack/react-table'
 import React from 'preact/compat';
 
 
@@ -33,22 +36,20 @@ const testData: Approach[] = [
 
 
 const HeaderWithTooltip = ({ text, tooltip }: { text: string, tooltip: string }) =>
-    <OverlayTrigger placement="bottom" overlay={<Tooltip>{tooltip}</Tooltip>}>
-        <span>{text}</span>
-    </OverlayTrigger>;
+    <Tooltip placement="bottom" title={tooltip}><span>{text}</span></Tooltip>;
 
 
 const FAA_PLATE_URL = "https://aeronav.faa.gov/d-tpp/";
 // TODO: don't hardcode this
 const CYCLE_NUMBER = "2407";
 
-const ApproachTitle = ({title, plate_file}: {title: string, plate_file: string}) => {
+const ApproachTitle = ({ title, plate_file }: { title: string, plate_file: string }) => {
     const plateUrl = `${FAA_PLATE_URL}${CYCLE_NUMBER}/${plate_file}`;
-    return <span>{title} <a target="_blank" href={plateUrl}><i class="bi bi-file-earmark-pdf"></i></a></span>
+    return <span>{title} <a target="_blank" href={plateUrl}><PictureAsPdfIcon/></a></span>
 }
 
 
-const APPROACH_TYPE_TO_COLOR: {[key: string]: string} = {
+const APPROACH_TYPE_TO_COLOR: { [key: string]: string } = {
     "ILS": "primary",
     "LOC": "warning",
 
@@ -58,17 +59,17 @@ const APPROACH_TYPE_TO_COLOR: {[key: string]: string} = {
     "GPS": "success",
 }
 
-const ApproachTypes = ({ types }: { types: string[] }) => 
-    <Stack direction="horizontal" gap={1}>
+const ApproachTypes = ({ types }: { types: string[] }) =>
+    <Stack direction="row" spacing={1}>
         {types.map(type => {
             const color = APPROACH_TYPE_TO_COLOR[type] || "secondary";
-            return <Badge pill bg={color} text={color == "warning" || color == "light" ? "dark" : "light"}>
-                {type}
-            </Badge>;
+            return <Chip label={type} color={color as any} />
         })}
     </Stack>;
 
-const columnHelper = createColumnHelper<Approach>();
+
+const columnHelper = createMRTColumnHelper<Approach>();
+
 const columns = [
     columnHelper.accessor('airport', {
         header: 'Airport'
@@ -76,23 +77,35 @@ const columns = [
     columnHelper.accessor('approach_name', {
         header: 'Approach Title',
         // Render the title with a link to the plate file.
-        cell: (cell) => <ApproachTitle title={cell.getValue()} plate_file={cell.row.original.plate_file} />
+        Cell: ({ cell, row }) => <ApproachTitle title={cell.getValue()} plate_file={row.original.plate_file} />
     }),
     columnHelper.accessor('types', {
         header: 'Types',
-        cell: (cell) => <ApproachTypes types={cell.getValue()} />
+        enableSorting: false,
+        filterVariant: 'multi-select',
+        filterSelectOptions: ["ILS", "LOC"],
+        Cell: ({ cell }) => <ApproachTypes types={cell.getValue()} />,
     }),
     columnHelper.group({
+        id: 'app_features',
         header: 'Approach Features',
         columns: [
             columnHelper.accessor('has_procedure_turn', {
-                header: () => <HeaderWithTooltip text="PT" tooltip="Procedure Turn" />,
+                header: "PT",
+                Header: <HeaderWithTooltip text="PT" tooltip="Procedure Turn" />,
+                enableSorting: false,
+                // TODO: show an icon of PT/HILPT/Arc here
+                Cell: ({ cell }) => <span>Hi</span>,
             }),
             columnHelper.accessor('has_hold_in_lieu_of_procedure_turn', {
-                header: () => <HeaderWithTooltip text="HILPT" tooltip="Hold-In-Lieu of Procedure Turn" />,
+                header: "HILPT",
+                Header: <HeaderWithTooltip text="HILPT" tooltip="Hold-In-Lieu of Procedure Turn" />,
+                enableSorting: false,
             }),
             columnHelper.accessor('has_dme_arc', {
-                header: () => <HeaderWithTooltip text="Arc" tooltip="DME Arc" />,
+                header: "Arc",
+                Header: <HeaderWithTooltip text="Arc" tooltip="DME Arc" />,
+                enableSorting: false,
             })
         ]
     }),
@@ -101,41 +114,12 @@ const columns = [
 
 export default function ApproachTable() {
     const [data, _setData] = React.useState(() => [...testData]);
-    const table = useReactTable({
-        data,
+    const table = useMaterialReactTable({
         columns,
-        getCoreRowModel: getCoreRowModel(),
+        data,
+        columnFilterDisplayMode: 'popover',
+        enableColumnActions: false,
     });
 
-    return <div style={{ height: "50vh", minHeight: "300px" }}>
-        <Table hover>
-            <thead>
-                {table.getHeaderGroups().map(headerGroup => (
-                    <tr key={headerGroup.id}>
-                        {headerGroup.headers.map(header => (
-                            <th key={header.id} colSpan={header.colSpan}>
-                                {header.isPlaceholder
-                                    ? null
-                                    : flexRender(
-                                        header.column.columnDef.header,
-                                        header.getContext()
-                                    )}
-                            </th>
-                        ))}
-                    </tr>
-                ))}
-            </thead>
-            <tbody>
-                {table.getRowModel().rows.map(row => (
-                    <tr key={row.id}>
-                        {row.getVisibleCells().map(cell => (
-                            <td key={cell.id}>
-                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                            </td>
-                        ))}
-                    </tr>
-                ))}
-            </tbody>
-        </Table>
-    </div>
+    return <MRT_Table table={table} />;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,4 @@
 import { render } from 'preact'
 import { App } from './app.tsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.min.css';
 
 render(<App />, document.getElementById('app')!)
