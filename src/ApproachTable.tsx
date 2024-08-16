0c04f3988e37bbb620c6aed47f593e6e29b9f7cd
Ammar Askar <ammar@ammaraskar.com>
2024-08-15 23:37:17 -0400

    Reformat ApproachTable

--- src/ApproachTable.tsx
+++ src/ApproachTable.tsx
@@ -8,12 +8,18 @@ import Paper from "@mui/material/Paper";
 import TableContainer from "@mui/material/TableContainer";
 import Table from "@mui/material/Table";
 import TableHead from "@mui/material/TableHead";
+import TableCell from "@mui/material/TableCell";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import Alert from "@mui/material/Alert";
+import Link from "@mui/material/Link";
 import {
   MaterialReactTable,
   useMaterialReactTable,
   createMRTColumnHelper,
   MRT_Row,
 } from "material-react-table";
+import { ColumnFiltersState } from "@tanstack/table-core";
 
 import React, { useEffect, useMemo, useState } from "preact/compat";
 import {
@@ -21,13 +27,13 @@ import {
   HoldInLieuIcon,
   ProcedureTurnIcon,
 } from "./ProcedureIcons";
-import { AirportsMap, Approach, ApproachMinimums, ApproachTypeString, MinimumsValue } from "./Approach";
-import TableCell from "@mui/material/TableCell";
-import TableBody from "@mui/material/TableBody";
-import TableRow from "@mui/material/TableRow";
-import Alert from "@mui/material/Alert";
-import Link from "@mui/material/Link";
-import { ColumnFiltersState } from "@tanstack/table-core";
+import {
+  AirportsMap,
+  Approach,
+  ApproachMinimums,
+  ApproachTypeString,
+  MinimumsValue,
+} from "./Approach";
 
 
 const HeaderWithTooltip = ({
@@ -77,184 +83,212 @@ const ApproachTypesCell = ({ types }: { types: ApproachTypeString[] }) => (
   </Stack>
 );
 
-const MinimumsCell = (props: {mins: MinimumsValue | "NA" | undefined}) => {
-    const mins = props.mins;
+const MinimumsCell = (props: { mins: MinimumsValue | "NA" | undefined }) => {
+  const mins = props.mins;
 
-    if (mins === "NA") {
-        return <Alert variant="outlined" severity="error">NA</Alert>
-    } else if (mins && mins.altitude) {
-        return <span>{mins.altitude}ft</span>
-    }
+  if (mins === "NA") {
+    return (
+      <Alert variant="outlined" severity="error">
+        NA
+      </Alert>
+    );
+  } else if (mins && mins.altitude) {
+    return <span>{mins.altitude}ft</span>;
+  }
 
-    return <i>(Parsing error)</i>;
-}
+  return <i>(Parsing error)</i>;
+};
 
-const ApproachMinimumsTable = (props: {minimums: ApproachMinimums[]}) => (
-    <TableContainer component={Paper}>
-        <Table>
-            <TableHead>
-                <TableRow>
-                    <TableCell>Category</TableCell>
-                    <TableCell>A</TableCell>
-                    <TableCell>B</TableCell>
-                    <TableCell>C</TableCell>
-                    <TableCell>D</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {props.minimums.map((mins, i) => (
-                    <TableRow key={i}>
-                        <TableCell>{mins.minimums_type}</TableCell>
-                        <TableCell><MinimumsCell mins={mins.cat_a} /></TableCell>
-                        <TableCell><MinimumsCell mins={mins.cat_b} /></TableCell>
-                        <TableCell><MinimumsCell mins={mins.cat_c} /></TableCell>
-                        <TableCell><MinimumsCell mins={mins.cat_d} /></TableCell>
-                    </TableRow>
-                ))}
-            </TableBody>
-        </Table>
-    </TableContainer>
+const ApproachMinimumsTable = (props: { minimums: ApproachMinimums[] }) => (
+  <TableContainer component={Paper}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Category</TableCell>
+          <TableCell>A</TableCell>
+          <TableCell>B</TableCell>
+          <TableCell>C</TableCell>
+          <TableCell>D</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {props.minimums.map((mins, i) => (
+          <TableRow key={i}>
+            <TableCell>{mins.minimums_type}</TableCell>
+            <TableCell>
+              <MinimumsCell mins={mins.cat_a} />
+            </TableCell>
+            <TableCell>
+              <MinimumsCell mins={mins.cat_b} />
+            </TableCell>
+            <TableCell>
+              <MinimumsCell mins={mins.cat_c} />
+            </TableCell>
+            <TableCell>
+              <MinimumsCell mins={mins.cat_d} />
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
 );
 
 const ApproachDetailPanel = (row: MRT_Row<Approach>, airports: AirportsMap) => {
-    const approach = row.original;
-    const airport = airports[approach.airport];
+  const approach = row.original;
+  const airport = airports[approach.airport];
 
-    const runway = airport.runways.filter(r => r.name == approach.runway).shift();
+  const runway = airport.runways
+    .filter((r) => r.name == approach.runway)
+    .shift();
 
-    return <Grid container spacing={2}>
-        <Grid item xs={5}>
-            <Typography variant="h4">{approach.approach_name}</Typography>
-            <Typography variant="subtitle1">{airport.name} ({approach.airport})</Typography>
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={5}>
+        <Typography variant="h4">{approach.approach_name}</Typography>
+        <Typography variant="subtitle1">
+          {airport.name} ({approach.airport})
+        </Typography>
 
-            {runway && 
-                <Typography variant="subtitle2">
-                    <Tooltip title="Runway Threshold Elevation"><span>RTE </span></Tooltip>
-                    {runway.threshold_elevation.toFixed(0)}ft MSL
-                </Typography>}
+        {runway && (
+          <Typography variant="subtitle2">
+            <Tooltip title="Runway Threshold Elevation">
+              <span>RTE </span>
+            </Tooltip>
+            {runway.threshold_elevation.toFixed(0)}ft MSL
+          </Typography>
+        )}
 
-            {approach.text_comments.length > 0 &&
-                <Paper sx={{p: 2, m: 2}} elevation={3}>
-                    <Typography variant="subtitle1">Comments</Typography>
-                    <Typography variant="body2">
-                        {approach.text_comments}
-                    </Typography>
-                </Paper>}
+        {approach.text_comments.length > 0 && (
+          <Paper sx={{ p: 2, m: 2 }} elevation={3}>
+            <Typography variant="subtitle1">Comments</Typography>
+            <Typography variant="body2">{approach.text_comments}</Typography>
+          </Paper>
+        )}
+      </Grid>
+
+      {approach.minimums.length > 0 && (
+        <Grid item xs={7}>
+          <Typography variant="subtitle1">Minimums</Typography>
+          <ApproachMinimumsTable minimums={approach.minimums} />
         </Grid>
-
-        {approach.minimums.length > 0 &&
-            <Grid item xs={7}>
-                <Typography variant="subtitle1">Minimums</Typography>
-                <ApproachMinimumsTable minimums={approach.minimums} />
-            </Grid>
-        }
-    </Grid>;
+      )}
+    </Grid>
+  );
 };
 
 const columnHelper = createMRTColumnHelper<Approach>();
 
-export default function ApproachTable(props: { dttpCycleNumber: string, data: Approach[], approachTypes: string[], airports: AirportsMap }) {
+export default function ApproachTable(props: {
+  dttpCycleNumber: string;
+  data: Approach[];
+  approachTypes: string[];
+  airports: AirportsMap;
+}) {
   const data = useMemo(() => props.data, [props.data]);
 
-  const columns = useMemo(() => [
-    columnHelper.group({
-      id: "approach",
-      header: "Approach",
-      columns: [
-        columnHelper.accessor("airport", {
-          header: "Airport",
-          size: 50,
-          muiFilterTextFieldProps: {
-            sx: {minWidth: '50px'}
-          },
-        }),
-        columnHelper.accessor("approach_name", {
-          header: "Approach Title",
-          enableSorting: false,
-          // Render the title with a link to the plate file.
-          Cell: ({ renderedCellValue, row }) => (
-            <ApproachTitle
-              dttpCycleNumber={props.dttpCycleNumber}
-              title={renderedCellValue}
-              plate_file={row.original.plate_file}
-            />
-          ),
-        }),
-        columnHelper.accessor("types", {
-          header: "Types",
-          enableSorting: false,
-          filterVariant: "multi-select",
-          filterSelectOptions: props.approachTypes,
-          Cell: ({ cell }) => <ApproachTypesCell types={cell.getValue()} />,
-        }),
-        columnHelper.accessor("distance", {
+  const columns = useMemo(
+    () => [
+      columnHelper.group({
+        id: "approach",
+        header: "Approach",
+        columns: [
+          columnHelper.accessor("airport", {
+            header: "Airport",
+            size: 50,
+            muiFilterTextFieldProps: {
+              sx: { minWidth: "50px" },
+            },
+          }),
+          columnHelper.accessor("approach_name", {
+            header: "Approach Title",
+            enableSorting: false,
+            // Render the title with a link to the plate file.
+            Cell: ({ renderedCellValue, row }) => (
+              <ApproachTitle
+                dttpCycleNumber={props.dttpCycleNumber}
+                title={renderedCellValue}
+                plate_file={row.original.plate_file}
+              />
+            ),
+          }),
+          columnHelper.accessor("types", {
+            header: "Types",
+            enableSorting: false,
+            filterVariant: "multi-select",
+            filterSelectOptions: props.approachTypes,
+            Cell: ({ cell }) => <ApproachTypesCell types={cell.getValue()} />,
+          }),
+          columnHelper.accessor("distance", {
             id: "distance",
             header: "Distance",
             size: 40,
             Cell: ({ cell }) => {
-                const val = cell.getValue<number | undefined>();
-                if (val !== undefined) {
-                    return `${val.toFixed(1)} NM`;
-                }
+              const val = cell.getValue<number | undefined>();
+              if (val !== undefined) {
+                return `${val.toFixed(1)} NM`;
+              }
             },
             enableColumnFilter: false,
-        }),
-      ],
-    }),
-    columnHelper.group({
-      id: "app_features",
-      header: "Approach Features",
-      columns: [
-        columnHelper.accessor("has_procedure_turn", {
-          header: "PT",
-          size: 40,
-          Header: <HeaderWithTooltip text="PT" tooltip="Procedure Turn" />,
-          enableSorting: false,
-          filterVariant: "checkbox",
-          muiFilterCheckboxProps: { title: "" },
-          Cell: ({ cell }) => (
-            <ProcedureTurnIcon
-              fontSize="medium"
-              color={cell.getValue<boolean>() ? "inherit" : "disabled"}
-            />
-          ),
-        }),
-        columnHelper.accessor("has_hold_in_lieu_of_procedure_turn", {
-          header: "HILPT",
-          size: 40,
-          Header: (
-            <HeaderWithTooltip
-              text="HILPT"
-              tooltip="Hold-In-Lieu of Procedure Turn"
-            />
-          ),
-          enableSorting: false,
-          filterVariant: "checkbox",
-          muiFilterCheckboxProps: { title: "" },
-          Cell: ({ cell }) => (
-            <HoldInLieuIcon
-              fontSize="medium"
-              color={cell.getValue<boolean>() ? "inherit" : "disabled"}
-            />
-          ),
-        }),
-        columnHelper.accessor("has_dme_arc", {
-          header: "Arc",
-          size: 40,
-          Header: <HeaderWithTooltip text="Arc" tooltip="DME Arc" />,
-          enableSorting: false,
-          filterVariant: "checkbox",
-          muiFilterCheckboxProps: { title: "" },
-          Cell: ({ cell }) => (
-            <DmeArcIcon
-              fontSize="medium"
-              color={cell.getValue<boolean>() ? "inherit" : "disabled"}
-            />
-          ),
-        }),
-      ],
-    }),
-  ], [props.dttpCycleNumber, props.approachTypes]);
+          }),
+        ],
+      }),
+      columnHelper.group({
+        id: "app_features",
+        header: "Approach Features",
+        columns: [
+          columnHelper.accessor("has_procedure_turn", {
+            header: "PT",
+            size: 40,
+            Header: <HeaderWithTooltip text="PT" tooltip="Procedure Turn" />,
+            enableSorting: false,
+            filterVariant: "checkbox",
+            muiFilterCheckboxProps: { title: "" },
+            Cell: ({ cell }) => (
+              <ProcedureTurnIcon
+                fontSize="medium"
+                color={cell.getValue<boolean>() ? "inherit" : "disabled"}
+              />
+            ),
+          }),
+          columnHelper.accessor("has_hold_in_lieu_of_procedure_turn", {
+            header: "HILPT",
+            size: 40,
+            Header: (
+              <HeaderWithTooltip
+                text="HILPT"
+                tooltip="Hold-In-Lieu of Procedure Turn"
+              />
+            ),
+            enableSorting: false,
+            filterVariant: "checkbox",
+            muiFilterCheckboxProps: { title: "" },
+            Cell: ({ cell }) => (
+              <HoldInLieuIcon
+                fontSize="medium"
+                color={cell.getValue<boolean>() ? "inherit" : "disabled"}
+              />
+            ),
+          }),
+          columnHelper.accessor("has_dme_arc", {
+            header: "Arc",
+            size: 40,
+            Header: <HeaderWithTooltip text="Arc" tooltip="DME Arc" />,
+            enableSorting: false,
+            filterVariant: "checkbox",
+            muiFilterCheckboxProps: { title: "" },
+            Cell: ({ cell }) => (
+              <DmeArcIcon
+                fontSize="medium"
+                color={cell.getValue<boolean>() ? "inherit" : "disabled"}
+              />
+            ),
+          }),
+        ],
+      }),
+    ],
+    [props.dttpCycleNumber, props.approachTypes],
+  );
 
   // Manual state management over column filtering so we can add a callback for
   // it.
@@ -265,23 +299,22 @@ export default function ApproachTable(props: { dttpCycleNumber: string, data: Ap
     data: data,
     enableTopToolbar: false,
     enableColumnActions: false,
-    initialState: { 
-        showColumnFilters: true,
-        density: "compact",
-        sorting: [{ id: 'distance', desc: false }]
+    initialState: {
+      showColumnFilters: true,
+      density: "compact",
+      sorting: [{ id: "distance", desc: false }],
     },
     renderDetailPanel: ({ row }) => ApproachDetailPanel(row, props.airports),
     onColumnFiltersChange: setColumnFilters,
     state: {
-        columnFilters
-    }
+      columnFilters,
+    },
   });
 
   // Callback for when filtering changes.
   useEffect(() => {
-    const filteredRows = table.getFilteredRowModel().flatRows.map(r => r.original);
+    const filteredRows = table.getFilteredRowModel().flatRows.map((r) => r.original);
   }, [columnFilters, props.data]);
-
 
   return <MaterialReactTable table={table} />;
 }
